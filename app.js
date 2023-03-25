781cdae037dea16f953b186d8b4767a87cd597bb
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-03-25 06:29:56 -0600

    completed api function

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ fetch("https://ipgeolocation.abstractapi.com/v1/?api_key=988347346abd482f8b13e24
 
     let address = data.ip_address;
     let addressResult = document.querySelector("#address-result");
-    addressResult.innerHTML = `${address}`
+    addressResult.innerHTML = `${address}`;
 
     location = data.location;
     let locationResult = document.querySelector("#location-result");
+    locationResult.innerHTML = `${location}`;
+
+    let timezone = data.timezone.edt;
     let timezoneResult = document.querySelector("#timezone-result");
+    timezoneResult.innerHTML = `${timezone}`;
+
+    let isp = data.connection.isp_name;
     let ispResult = document.querySelector("#isp-result");
+    ispResult.innerHTML = `${isp}`
 })
 
